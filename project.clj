1a063a35f62640291952994e6a18719d9941f8c4
Mihai Giurgeanu <mihai.giurgeanu@gmail.com>
2015-02-22 20:16:55 +0200

    Updated project info.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject httpprobe "0.1.2"
-  :description "Sends many http GET request"
+(defproject httpcrawler "0.1.0-SNAPSHOT"
+  :description "Very fast web crawler based on http-kit"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
